81247658485d5721279fb4a24bab874164c4c143
Sathanan <112382296+Sathanan@users.noreply.github.com>
2023-05-19 23:39:05 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Meine Webseite</title>
+  <style>
+    /* Hier kommt der CSS-Code für das Styling der Webseite */
+    body {
+      font-family: Arial, sans-serif;
+      margin: 0;
+      padding: 0;
+    }
+
+    header {
+      background-color: #333;
+      color: #fff;
+      padding: 20px;
+    }
+
+    main {
+      padding: 20px;
+    }
+
+    footer {
+      background-color: #333;
+      color: #fff;
+      padding: 20px;
+    }
+  </style>
+</head>
+<body>
+  <header>
+    <h1>Willkommen auf meiner Webseite</h1>
+    <p>Hier finden Sie Informationen zu verschiedenen Themen.</p>
+  </header>
+
+  <main>
+    <h2>Über uns</h2>
+    <p>Wir sind ein Team von Experten, das Ihnen die neuesten Informationen und Ressourcen zu den interessantesten Themen zur Verfügung stellt.</p>
+
+    <h2>Unsere Dienstleistungen</h2>
+    <ul>
+      <li>Webdesign und Entwicklung</li>
+      <li>Content-Erstellung und -Optimierung</li>
+      <li>Suchmaschinenoptimierung</li>
+      <li>Social-Media-Marketing</li>
+      <li>Online-Werbekampagnen</li>
+    </ul>
+
+    <h2>Unsere Mission</h2>
+    <p>Unsere Mission ist es, Unternehmen und Einzelpersonen dabei zu helfen, ihre Online-Präsenz zu verbessern und ihre Ziele im digitalen Raum zu erreichen. Wir glauben an die Kraft des Internets und sind leidenschaftlich daran interessiert, unseren Kunden dabei zu helfen, erfolgreich zu sein.</p>
+  </main>
+
+  <footer>
+    <p>&copy; 2023 Meine Webseite. Alle Rechte vorbehalten.</p>
+  </footer>
+</body>
+</html>
